0531f6cc3be0d9b5ad3ee6fb14f7ee56bdc77211
Sameer Chawla <sameerchawla@wireless-10-104-238-179.umd.edu>
2024-02-18 20:13:31 -0500

    what

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import Home from './components/Home.vue'
+</script>
+
+<template>
+  <div class="custom-bg">
+    <header>
+      <Home/>
+    </header>
+
+    <main>
+    </main>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
